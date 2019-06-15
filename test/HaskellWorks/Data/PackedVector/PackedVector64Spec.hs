91c46e20ba51ca74f5219e79c51ef3679b2cd7da
John Ky <newhoggy@gmail.com>
2019-06-15 17:06:33 +1000

    New fromListN function

--- test/HaskellWorks/Data/PackedVector/PackedVector64Spec.hs
+++ test/HaskellWorks/Data/PackedVector/PackedVector64Spec.hs
@@ -1,28 +1,34 @@
 module HaskellWorks.Data.PackedVector.PackedVector64Spec (spec) where
 
-import HaskellWorks.Data.AtIndex
+import HaskellWorks.Data.AtIndex                     ((!!!))
 import HaskellWorks.Data.PackedVector.PackedVector64
+import HaskellWorks.Hspec.Hedgehog
 import Hedgehog
 import Test.Hspec
--- import Test.QuickCheck
-import HaskellWorks.Hspec.Hedgehog
 
-import qualified HaskellWorks.Gen          as G
-import qualified Hedgehog.Gen              as G
-import qualified Hedgehog.Range            as R
+import qualified HaskellWorks.Data.AtIndex                     as HW
+import qualified HaskellWorks.Data.PackedVector.PackedVector64 as PV
+import qualified HaskellWorks.Gen                              as G
+import qualified Hedgehog.Gen                                  as G
+import qualified Hedgehog.Range                                as R
 
 {-# ANN module ("HLint: Ignore Redundant do" :: String) #-}
 
 spec :: Spec
 spec = describe "HaskellWorks.Data.PackedVector.PackedVector64Spec" $ do
-  it "Round trip from list" $ require $ property $ do
+  it "fromList/toList works" $ require $ property $ do
     wSize <- forAll $ G.subWordSize 64
     len   <- forAll $ G.int   (R.linear 1 128)
     ws    <- forAll $ G.list  (R.singleton len) (G.word64OfSize wSize)
     toList (fromList wSize ws) === ws
-  it "Round trip from list" $ require $ property $ do
+  it "fromListN/toList works" $ require $ property $ do
+    wSize <- forAll $ G.subWordSize 64
+    len   <- forAll $ G.int   (R.linear 1 128)
+    ws    <- forAll $ G.list  (R.singleton len) (G.word64OfSize wSize)
+    toList (fromListN (HW.length ws) wSize ws) === ws
+  it "Index lookup works" $ require $ property $ do
     wSize <- forAll $ G.subWordSize 64
     len   <- forAll $ G.int   (R.linear 1 32)
     ws    <- forAll $ G.list  (R.singleton len) (G.word64OfSize wSize)
     i     <- forAll $ G.int64 (R.linear 0 (fromIntegral len - 1))
-    (fromList wSize ws !!! i) === (ws !!! i)
+    (PV.fromList wSize ws !!! i) === (ws !!! i)
