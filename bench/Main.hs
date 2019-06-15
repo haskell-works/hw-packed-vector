db4dbc39867786653cd074c14a0efd5204ad4d35
John Ky <newhoggy@gmail.com>
2019-06-15 17:32:13 +1000

    Benchmark atIndex

--- bench/Main.hs
+++ bench/Main.hs
@@ -10,18 +10,21 @@ import Data.Word
 import System.Directory
 
 import qualified Data.Vector.Storable                as DVS
+import qualified HaskellWorks.Data.AtIndex           as HW
 import qualified HaskellWorks.Data.FromForeignRegion as IO
-import qualified HaskellWorks.Data.Length            as HW
 import qualified HaskellWorks.Data.PackedVector      as PV
+
 
 makeBenchW64s :: IO [Benchmark]
 makeBenchW64s = do
   entries <- listDirectory "data/bench"
   let files = ("data/bench/" ++) <$> (".word64s" `isSuffixOf`) `filter` entries
   benchmarks <- forM files $ \file -> return
-    [ env (IO.mmapFromForeignRegion file) $ \(v :: DVS.Vector Word64) -> bgroup "blah" $ mempty
-      <> [bench ("DVS.fromList"  <> file) (whnf (PV.fromList                10) (DVS.toList v))]
-      <> [bench ("DVS.fromListN" <> file) (whnf (PV.fromListN (HW.length v) 10) (DVS.toList v))]
+    [ env (IO.mmapFromForeignRegion file) $ \(v :: DVS.Vector Word64) -> bgroup "with Vector64" $ mempty
+      <> [bench ("PV.fromList"  <> file) (whnf (PV.fromList                10) (DVS.toList v))]
+      <> [bench ("PV.fromListN" <> file) (whnf (PV.fromListN (HW.length v) 10) (DVS.toList v))]
+    , env (fmap (PV.fromList 10 . DVS.toList) (IO.mmapFromForeignRegion file)) $ \pv -> bgroup "with PackedVector64" $ mempty
+      <> [bench ("PV.atIndex "  <> file) (whnf (HW.atIndex pv) 20)]
     ]
   return (join benchmarks)
 
